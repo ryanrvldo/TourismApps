8794bc66b62d02fae9574928e0b162e6762d706d
malaikatmaut <ryanrvldo.linux@gmail.com>
2020-10-21 22:27:30 +0700

    implements MultiBinding to ViewModelFactory.kt

--- app/src/main/java/com/ryanrvldo/tourismapp/core/ui/ViewModelFactory.kt
+++ app/src/main/java/com/ryanrvldo/tourismapp/core/ui/ViewModelFactory.kt
@@ -2,30 +2,20 @@ package com.ryanrvldo.tourismapp.core.ui
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.ryanrvldo.tourismapp.core.domain.usecase.TourismUseCase
-import com.ryanrvldo.tourismapp.detail.DetailTourismViewModel
 import com.ryanrvldo.tourismapp.di.AppScope
-import com.ryanrvldo.tourismapp.favorite.FavoriteViewModel
-import com.ryanrvldo.tourismapp.home.HomeViewModel
 import javax.inject.Inject
+import javax.inject.Provider
 
 @AppScope
 class ViewModelFactory @Inject constructor(
-    private val tourismUseCase: TourismUseCase
+    private val creators: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>
 ) : ViewModelProvider.NewInstanceFactory() {
 
     @Suppress("UNCHECKED_CAST")
-    override fun <T : ViewModel> create(modelClass: Class<T>): T =
-        when {
-            modelClass.isAssignableFrom(HomeViewModel::class.java) -> {
-                HomeViewModel(tourismUseCase) as T
-            }
-            modelClass.isAssignableFrom(FavoriteViewModel::class.java) -> {
-                FavoriteViewModel(tourismUseCase) as T
-            }
-            modelClass.isAssignableFrom(DetailTourismViewModel::class.java) -> {
-                DetailTourismViewModel(tourismUseCase) as T
-            }
-            else -> throw Throwable("Unknown ViewModel class: " + modelClass.name)
-        }
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        val creator = creators[modelClass] ?: creators.entries.firstOrNull {
+            modelClass.isAssignableFrom(it.key)
+        }?.value ?: throw IllegalArgumentException("unknown model class $modelClass")
+        return creator.get() as T
+    }
 }
--- app/src/main/java/com/ryanrvldo/tourismapp/detail/DetailTourismViewModel.kt
+++ app/src/main/java/com/ryanrvldo/tourismapp/detail/DetailTourismViewModel.kt
@@ -3,8 +3,11 @@ package com.ryanrvldo.tourismapp.detail
 import androidx.lifecycle.ViewModel
 import com.ryanrvldo.tourismapp.core.domain.model.Tourism
 import com.ryanrvldo.tourismapp.core.domain.usecase.TourismUseCase
+import javax.inject.Inject
 
-class DetailTourismViewModel(private val tourismUseCase: TourismUseCase) : ViewModel() {
+class DetailTourismViewModel @Inject constructor(
+    private val tourismUseCase: TourismUseCase
+) : ViewModel() {
 
     fun setFavoriteTourism(tourism: Tourism, newStatus: Boolean) =
         tourismUseCase.setFavoriteTourism(tourism, newStatus)
