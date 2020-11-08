2b6c46213157d924f310d7d0869ab44ce4072484
malaikatmaut <ryanrvldo.linux@gmail.com>
2020-11-08 23:28:17 +0700

    implements modularization

--- core/src/main/java/com/ryanrvldo/tourismapp/core/data/NetworkBoundResource.kt
+++ core/src/main/java/com/ryanrvldo/tourismapp/core/data/NetworkBoundResource.kt
@@ -14,16 +14,32 @@ abstract class NetworkBoundResource<ResultType, RequestType>(private val mExecut
             when (val apiResponse = createCall().first()) {
                 is ApiResponse.Success -> {
                     saveCallResult(apiResponse.data)
-                    emitAll(loadFromDB().map { Resource.Success(it) })
+                    emitAll(loadFromDB().map {
+                        Resource.Success(
+                            it
+                        )
+                    })
                 }
-                is ApiResponse.Empty -> emitAll(loadFromDB().map { Resource.Success(it) })
+                is ApiResponse.Empty -> emitAll(loadFromDB().map {
+                    Resource.Success(
+                        it
+                    )
+                })
                 is ApiResponse.Error -> {
                     onFetchFailed()
-                    emit(Resource.Error<ResultType>(apiResponse.errorMessage))
+                    emit(
+                        Resource.Error<ResultType>(
+                            apiResponse.errorMessage
+                        )
+                    )
                 }
             }
         } else {
-            emitAll(loadFromDB().map { Resource.Success(it) })
+            emitAll(loadFromDB().map {
+                Resource.Success(
+                    it
+                )
+            })
         }
     }
 
